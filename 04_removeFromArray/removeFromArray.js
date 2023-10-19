94ae3695f194025a4242169cdb21aad7353c3556
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-18 20:17:22 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rest) {
+    let newArr = arr;
+    for(i = 0; i < rest.length; i++){
+        let itemIndex = newArr.indexOf(rest[i]);
+        if (itemIndex === -1){
+            continue;
+        }
+        let arrCopy = newArr.slice();
+        arrCopy.splice(itemIndex, 1);
+        newArr = arrCopy;
+    }
+    
+    return newArr;
 };
 
 // Do not edit below this line
