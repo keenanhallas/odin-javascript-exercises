38eee75b6e6aace49b06179fca154652aba1264e
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-18 21:14:50 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = 5/9 * (fTemp - 32);
+  cTemp = Math.round(cTemp * 10) / 10;
+  return cTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fTemp = cTemp / (5/9) + 32;
+  fTemp = Math.round(fTemp * 10) / 10;
+  return fTemp;
 };
 
 // Do not edit below this line
