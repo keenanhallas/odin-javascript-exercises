a828d95bb967025ede2c41cf97256b8a57eeee85
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-18 20:49:24 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+    let sum = 0;
+    let big = num1 > num2 ? num1 : num2;
+    let small = num2 < num1 ? num2 : num1;
+    for (i = small; i <= big; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
