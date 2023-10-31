1c855696f3d1e88a79ef2dbeac2743f2bc95902e
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-31 12:07:02 -0500

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let chars = string.split("");
 
+    let letters = chars.filter(char => {
+        return (char > "@" && char < "[" ||
+                char > "`" && char < "{") ||
+                char > "/" && char < ";";
+    });
+
+    let lowerCase = letters.map(letter => {
+        return letter.toLowerCase();
+    });
+
+    let forward = lowerCase.join("");
+    
+    lowerCase.reverse();
+    let backward = lowerCase.join("");
+
+    if (forward === backward){
+        return true;
+    } else return false
 };
 
 // Do not edit below this line
