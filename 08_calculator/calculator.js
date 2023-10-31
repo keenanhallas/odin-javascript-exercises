d8bf49de8a89bd56e5fc684e014b27dc9be3c185
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-31 11:18:37 -0500

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, number) => {
+    return total + number;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, number) => {
+    return total * number;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  //Is the fact that !0 = 1 an exception that has to be handled outside of the regular program logic, like with this test?
+  if(number === 0) return 1;
+
+  let total = 1;
+  for(let i = 1; i <= number; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
