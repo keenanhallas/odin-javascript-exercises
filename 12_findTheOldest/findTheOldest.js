81d544fdd1dafd0b156895e3b8af599e61165961
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-31 15:44:05 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    return people.sort((a, b) => {
+        let aAge = a.yearOfDeath ?
+            a.yearOfDeath - a.yearOfBirth :
+            currentYear - a.yearOfBirth;
+        let bAge = b.yearOfDeath ?
+            b.yearOfDeath - b.yearOfBirth :
+            currentYear - b.yearOfBirth;
+        return bAge - aAge;
+    })[0];
 };
 
 // Do not edit below this line
