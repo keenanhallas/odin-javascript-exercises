b66f96cff677c4869c50a5c69b33de696e5d603a
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-31 14:37:41 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,40 @@
-const fibonacci = function() {
+const fibonacci = function(target) {
+    if(target < 0) return "OOPS";
+    if(isNaN(parseInt(target))) return "OOPS";
 
+    //Focus on 3 elements of the sequence at once
+    let currentSequence = {
+        1: 1,
+        2: 1,
+        3: 2,
+    };
+
+    let firstKey = 1;
+    let secondKey = 2;
+    let thirdKey = 3;
+
+    //While target is not in current sequence
+    while(!currentSequence[target]){
+        //Define new values
+        let firstValue = currentSequence[secondKey];
+        let secondValue = currentSequence[thirdKey];
+        //Calculate the new third value by adding the new first & new second values
+        let thirdValue = firstValue + secondValue;
+
+        //Update keys
+        firstKey++;
+        secondKey++;
+        thirdKey++;
+
+        currentSequence = {
+            [firstKey]: firstValue,
+            [secondKey]: secondValue,
+            [thirdKey]: thirdValue
+        };
+    };
+
+    //If the target is a key in the currentSequence object, return that key's value
+    return currentSequence[target];
 };
 
 // Do not edit below this line
