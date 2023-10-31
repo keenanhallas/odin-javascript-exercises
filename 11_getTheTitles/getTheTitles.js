8049a4c2c175ad8d9df65b9a303298637a3a2eaf
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2023-10-31 15:02:58 -0500

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.map(book => book.title);
+    // This works too, but is not standard practice
+    // return books.reduce((titles, book) => {
+    //     titles.push(book.title);
+    //     return titles;
+    // }, []);
 };
 
 // Do not edit below this line
